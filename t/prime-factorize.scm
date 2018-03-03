65497dc809dadfc5c0868a2f6e1892bd9b1ba60a
rain1 <rain1@airmail.cc>
2018-03-03 21:52:43 +0000

    add a failing test

---
+++ t/prime-factorize.scm
@@ -0,0 +1,36 @@
+(define (for-each proc l)
+  (if (null? l)
+      #t
+      (begin (proc (car l))
+	     (for-each proc (cdr l)))))
+
+(define (factor^ divisor number)
+(print divisor)
+(print number)
+    (if (> (* divisor divisor) number)
+        (cons number '())
+        (if (= (modulo number divisor) 0)
+            (cons divisor (factor^ divisor (quotient number divisor)))
+            (factor^ (+ divisor 1) number))))
+
+(define (factor number)
+(print "it starts")
+  (factor^ 2 number))
+
+(define (print-sp x) (print x) (print " "))
+
+(define (print-list l)
+  (for-each print-sp l)
+  (newline))
+
+(print-list (factor 111111111111))
+(print-list (factor 111111111111))
+(print-list (factor 111111111111))
+(print-list (factor 1111111111111))
+(print-list (factor 11111111111111))
+(print-list (factor 111111111111111))
+(print-list (factor 1111111111111111))
+(print-list (factor 11111111111111111))
+(print-list (factor 111111111111111111))
+;; (print-list (factor 1111111111111111111)) ;; this ones prime, so takes a long time
+;(print-list (factor 1111111111111111110)) ;; this is fast though
