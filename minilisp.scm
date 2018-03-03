fffb94b1f7e2f21c660bc44918e40f90851ea454
rain1 <rain1@airmail.cc>
2018-03-03 19:43:37 +0000

    add LIST macro

--- minilisp.scm
+++ minilisp.scm
@@ -16,3 +16,11 @@
 (defmacro let (table . body)
   (cons (cons 'lambda (cons (map car table) body))
         (map cadr table)))
+
+(define (list-maker elts)
+  (if (null? elts)
+      ''()
+      (cons 'cons (cons (car elts) (cons (list-maker (cdr elts)) '())))))
+
+(defmacro list (x . xs)
+  (list-maker (cons x xs)))
